e84b69e60f4522112077c29038cd7df9a5422d3a
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-06-10 13:10:11 +0530

    Time: 0 ms (100%), Space: 46.9 MB (13.12%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,28 +11,25 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        
 
         ListNode slow = head;
         ListNode fast = head;
 
         while(fast!=null && fast.next!=null){
-            
-
             slow = slow.next;
             fast = fast.next.next;
+
             if(slow==fast){
                 slow = head;
                 while(slow!=fast){
-                    fast = fast.next;
                     slow = slow.next;
-                    }
-                    return fast;
+                    fast = fast.next;
                 }
-                
-        
+                return fast;
             }
 
-        
+        }
         return null;
     }
 }
