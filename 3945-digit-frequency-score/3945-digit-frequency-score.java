e9011ad9c857722b4459fb7a8da65eef565942c8
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-06-11 23:20:22 +0530

    Time: 1 ms (99.31%), Space: 42.5 MB (85.36%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+
+
+        int res = 0;
+
+        while(n!=0){
+            res+=n%10;
+            n=n/10;
+        }
+        return res;
+        
+    }
+}
