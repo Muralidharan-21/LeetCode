9c071be785b725395c976839b17c6b6278caf1f4
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-03 15:14:07 +0530

    Time: 3 ms (81.83%), Space: 43.8 MB (14.08%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,44 +1,91 @@
 class Solution {
     public int calPoints(String[] operations) {
-        
-        Stack<Integer> stack = new Stack<>();
-        int res = 0;
-        int score = 0;
-        for(String ch : operations){
-            if(ch.equals("+")){
-                int temp = 0;
-                int f =stack.pop();
-                int s =stack.pop();
-                temp+=f;
-                temp+=s;
 
-                stack.push(s);
-                stack.push(f);
-                stack.push(temp);
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for(String s : operations){
+            
+            if(s.equals("+")){
+               int l = stack.pop();
+               int sl = stack.pop();
+
+               stack.push(sl);
+               stack.push(l);
+               stack.push(l+sl);
             }
-            else if(ch.equals("D")){
-                
-                int pop = stack.pop();
-                int temp = pop * 2;
-                stack.push(pop);
-                stack.push(temp);
-                
+            else if(s.equals("D")){
+                stack.push(stack.peek()*2);
             }
-            else if(ch.equals("C")){
-                stack.pop();
-            }
+
+            else if(s.equals("C")){stack.pop();}
+
 
             else{
-            
-            score = Integer.parseInt(ch);
-            stack.push(score);
+                stack.push(Integer.parseInt(s));
             }
-        }
 
-        for(int num : stack){
-            res+=num;
         }
-        return res;
-
+        for(int i : stack){
+            sum+=i;
+        }
+        return sum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int calPoints(String[] operations) {
+        
+//         Stack<Integer> stack = new Stack<>();
+//         int res = 0;
+//         int score = 0;
+//         for(String ch : operations){
+//             if(ch.equals("+")){
+//                 int temp = 0;
+//                 int f =stack.pop();
+//                 int s =stack.pop();
+//                 temp+=f;
+//                 temp+=s;
+
+//                 stack.push(s);
+//                 stack.push(f);
+//                 stack.push(temp);
+//             }
+//             else if(ch.equals("D")){
+                
+//                 int pop = stack.pop();
+//                 int temp = pop * 2;
+//                 stack.push(pop);
+//                 stack.push(temp);
+                
+//             }
+//             else if(ch.equals("C")){
+//                 stack.pop();
+//             }
+
+//             else{
+            
+//             score = Integer.parseInt(ch);
+//             stack.push(score);
+//             }
+//         }
+
+//         for(int num : stack){
+//             res+=num;
+//         }
+//         return res;
+
+//     }
+// }
