9eceb91c1385a32fffb8d1a4ef8b6fd9cbde2a6e
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-03 14:32:12 +0530

    Time: 3 ms (87.34%), Space: 43.5 MB (26.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,37 +3,65 @@ class Solution {
 
         Stack<Character> stack = new Stack<>();
 
-
         for(char ch : s.toCharArray()){
 
-            if(ch == '(' || ch == '[' || ch == '{'){
-                stack.push(ch);
-            }
+            if(!stack.isEmpty()
+              && ((ch == ')' && stack.peek()=='(' )||( ch == ']' && stack.peek()=='[') ||( ch=='}' && stack.peek()=='{'))){
+                stack.pop();
+              }
 
             else{
-
-                if(stack.isEmpty()){
-                    return false;
-                }
-
-                char top = stack.pop();
-
-                if(top == '(' && ch!= ')'){
-                    return false;
-                }
-                if(top == '[' && ch!= ']'){
-                    return  false;
-                }
-                if(top == '{' && ch != '}'){
-                    return false;
-                }
-
-
+                stack.push(ch);
             }
+              
+        
+
         }
 
         return stack.isEmpty();
-
-
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isValid(String s) {
+
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for(char ch : s.toCharArray()){
+
+//             if(ch == '(' || ch == '[' || ch == '{'){
+//                 stack.push(ch);
+//             }
+
+//             else{
+
+//                 if(stack.isEmpty()){return false;}
+                
+//                 char top = stack.pop();
+//                 if(top == '(' && ch != ')'){return false;}
+//                 if(top == '{' && ch != '}'){return false;}
+//                 if(top == '[' && ch != ']'){return false;}
+//             }
+
+
+//         }
+//         return stack.isEmpty();
+//     }
+// }
