710bb3eac740feb61eea1defc9bf5fad77848a31
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-17 22:27:35 +0530

    Time: 15 ms (61.53%), Space: 48.2 MB (75.45%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,58 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        
-        Stack<Integer> stack = new Stack<>();
+
         int n = nums.length;
-        int res[] = new int[n];
+        int[] res = new int[n];
         Arrays.fill(res,-1);
 
-        for(int i = 2*n -1 ; i>=0 ; i--){
+        Stack<Integer> stack = new Stack<>();
 
+        for(int i = n * 2 -1;i>=0;i--){
             int idx = i%n;
 
-            while(!stack.isEmpty() && nums[idx]>= stack.peek()){
+            while(!stack.isEmpty() && nums[idx]>=stack.peek()){
                 stack.pop();
             }
-
             if(i<n){
-                res[i] =   stack.isEmpty() ? -1 : stack.peek();
+                res[i] = stack.isEmpty() ? -1 : stack.peek();  
             }
-
             stack.push(nums[idx]);
         }
-
         return res;
-
+        
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+        
+//         Stack<Integer> stack = new Stack<>();
+//         int n = nums.length;
+//         int res[] = new int[n];
+//         Arrays.fill(res,-1);
+
+//         for(int i = 2*n -1 ; i>=0 ; i--){
+
+//             int idx = i%n;
+
+//             while(!stack.isEmpty() && nums[idx]>= stack.peek()){
+//                 stack.pop();
+//             }
+
+//             if(i<n){
+//                 res[i] =   stack.isEmpty() ? -1 : stack.peek();
+//             }
+
+//             stack.push(nums[idx]);
+//         }
+
+//         return res;
+
+//     }
+// }
