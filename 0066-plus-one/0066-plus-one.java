acc8ab0084691965a0c82173482d670a51b218bb
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-28 12:07:14 +0530

    Time: 0 ms (100%), Space: 43.2 MB (79.91%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for(int i = n - 1 ; i>=0;i--){
+
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+
+        }
+
+        int  arr[] = new int[n+1];
+        arr[0] = 1;
+        return arr;
+
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+
+//         int n = digits.length;
+        
+//         for (int i = n -1; i>=0;i--){
+//             if(digits[i]<9){
+//                 digits[i]++;
+//                 return digits;
+//             }
+//             digits[i] = 0;
+//         }
+
+//         // if all were  9 // 99999 - 100000
+
+//         int[] arr = new int[n+1];
+//         arr[0]=1;
+//         return arr;
+//     }
+// }
