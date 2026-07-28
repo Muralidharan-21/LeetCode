94da30fc7551d887d6c7ecff459c33631aaf8815
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-28 17:26:45 +0530

    Time: 10 ms (43.04%), Space: 47.4 MB (7.57%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char x : magazine.toCharArray()){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+
+        for(char x : ransomNote.toCharArray()){
+
+            if(!map.containsKey(x)){
+                return false;
+            }
+            else{
+                map.put(x,map.get(x)-1);
+                if(map.get(x)==0){
+                    map.remove(x);
+                }
+            }
+        }
+        return true;
+
+    }
+}
