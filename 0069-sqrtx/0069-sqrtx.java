2f9e6a15b43be91345cd54491a67b68481d277a3
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-28 22:20:07 +0530

    Time: 1 ms (97.42%), Space: 42.6 MB (62.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        
+        int low = 1;
+        int high = x;
+        int ans = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            // Using division prevents integer overflow
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                high = mid - 1;
+            } else {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
