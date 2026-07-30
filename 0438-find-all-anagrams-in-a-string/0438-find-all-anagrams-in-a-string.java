d20360986ea00a0f699560a59ffcf858c2bc5905
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-07-30 16:47:22 +0530

    Time: 10 ms (80.72%), Space: 46.6 MB (84.17%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,22 +1,63 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
+        int pat[] = new int[26];
+        int win[] = new int[26];
 
         ArrayList<Integer> arr = new ArrayList<>();
-        char[] pp = p.toCharArray();
-        Arrays.sort(pp);
 
-        for(int i = 0 ; i<= s.length() - p.length() ;i++){
+        for(char x : p.toCharArray()){
+            pat[x-'a']++;
+        }
+        int winSize = p.length();
+        if(p.length()>s.length()){
+            return arr;
+        }
+        for(int i = 0 ; i<winSize ; i++){
+            win[s.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(pat,win)){
+            arr.add(0);
+        }
 
-            String sub = s.substring(i,i+p.length());
-            char[] temp = sub.toCharArray();
-            Arrays.sort(temp);
+        for(int i = winSize; i<s.length();i++){
 
-            if(Arrays.equals(temp,pp)){
-                arr.add(i);
+            win[s.charAt(i)-'a']++;
+            win[s.charAt(i-winSize)-'a']--;
+
+            if(Arrays.equals(pat,win)){
+                arr.add((i-winSize)+1);
             }
+
         }
         return arr;
         
     }
 }
+
+
+
+
+
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+
+
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         char[] pp = p.toCharArray();
+//         Arrays.sort(pp);
+
+//         for(int i = 0 ; i<= s.length() - p.length() ;i++){
+
+//             String sub = s.substring(i,i+p.length());
+//             char[] temp = sub.toCharArray();
+//             Arrays.sort(temp);
+
+//             if(Arrays.equals(temp,pp)){
+//                 arr.add(i);
+//             }
+//         }
+//         return arr;
+        
+//     }
+// }
