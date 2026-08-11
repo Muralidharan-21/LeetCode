7853957af28bc15a0ec2304303f26fa7123406b7
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-11 18:40:40 +0530

    Time: 15 ms (61.6%), Space: 48.8 MB (13.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-        int n = nums.length;
-        int[] res = new int[n];
-        Arrays.fill(res,-1);
-
         Stack<Integer> stack = new Stack<>();
+        int n = nums.length;
+        int[] res = new int[nums.length];
 
-        for(int i = n * 2 -1;i>=0;i--){
-            int idx = i%n;
+        for(int i = 2*n-1 ;i>=0 ; i--) {
+            int idx = i % n;
 
-            while(!stack.isEmpty() && nums[idx]>=stack.peek()){
+            while(!stack.isEmpty() && nums[idx]>= stack.peek()){
                 stack.pop();
             }
+           
             if(i<n){
-                res[i] = stack.isEmpty() ? -1 : stack.peek();  
+                res[i] = stack.isEmpty() ? -1 : stack.peek();
             }
-            stack.push(nums[idx]);
+
+             stack.push(nums[idx]);
+
+
         }
         return res;
         
@@ -29,30 +31,58 @@ class Solution {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
 //     public int[] nextGreaterElements(int[] nums) {
-        
-//         Stack<Integer> stack = new Stack<>();
+
 //         int n = nums.length;
-//         int res[] = new int[n];
+//         int[] res = new int[n];
 //         Arrays.fill(res,-1);
 
-//         for(int i = 2*n -1 ; i>=0 ; i--){
+//         Stack<Integer> stack = new Stack<>();
 
+//         for(int i = n * 2 -1;i>=0;i--){
 //             int idx = i%n;
 
-//             while(!stack.isEmpty() && nums[idx]>= stack.peek()){
+//             while(!stack.isEmpty() && nums[idx]>=stack.peek()){
 //                 stack.pop();
 //             }
-
 //             if(i<n){
-//                 res[i] =   stack.isEmpty() ? -1 : stack.peek();
+//                 res[i] = stack.isEmpty() ? -1 : stack.peek();  
 //             }
-
 //             stack.push(nums[idx]);
 //         }
-
 //         return res;
-
+        
 //     }
 // }
