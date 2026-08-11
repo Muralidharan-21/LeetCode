70e520380773c94fb1d6089280f54a28e833437b
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-11 17:58:58 +0530

    Time: 4 ms (74.14%), Space: 45 MB (76.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,24 +2,82 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
         HashMap<Integer,Integer> map = new HashMap<>();
+
         Stack<Integer> stack = new Stack<>();
 
-        for(int i = nums2.length-1 ; i>=0;i--){
-
-            while(!stack.isEmpty() && nums2[i]>=stack.peek()){
+        for(int i = nums2.length-1 ; i>=0 ; i--){
+            while(!stack.isEmpty() && nums2[i] >= stack.peek()){
                 stack.pop();
             }
-            map.put(nums2[i],stack.isEmpty()?-1 : stack.peek());
+            int x = stack.isEmpty() ? -1 : stack.peek();
+            map.put(nums2[i],x);
             stack.push(nums2[i]);
         }
 
         int[] res = new int[nums1.length];
-        int p = 0;
-        
+        int k = 0; 
         for(int x : nums1){
-            res[p++] = map.get(x);
+            res[k++] = map.get(x);
         }
+
         return res;
+       
+
+
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         Stack<Integer> stack = new Stack<>();
+
+//         for(int i = nums2.length-1 ; i>=0;i--){
+
+//             while(!stack.isEmpty() && nums2[i]>=stack.peek()){
+//                 stack.pop();
+//             }
+//             map.put(nums2[i],stack.isEmpty()?-1 : stack.peek());
+//             stack.push(nums2[i]);
+//         }
+
+//         int[] res = new int[nums1.length];
+//         int p = 0;
+        
+//         for(int x : nums1){
+//             res[p++] = map.get(x);
+//         }
+//         return res;
+        
+//     }
+// }
