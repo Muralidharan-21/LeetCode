f902eca6c0470c688d2f56ddb40493c81ba83024
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-13 14:11:52 +0530

    Time: 13 ms (80.03%), Space: 47.7 MB (49.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,74 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int n : nums){
-            map.put(n,map.getOrDefault(n,0)+1);
+        for(int x : nums){
+            map.put(x,map.getOrDefault(x,0)+1);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->a[1] - b[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1]-b[1]);
 
         for(int key : map.keySet()){
-            int freq  = map.get(key);
-            pq.add(new int[]{key,freq});
+
+            int freq = map.get(key);
+            pq.add(new int[] {key,freq});
 
             if(pq.size()>k){
                 pq.poll();
             }
-        }
-        int res[] = new int[k];
-        // int z = 0;
 
-        for(int i = 0; i<k ; i++){
+        }
+
+        int[] res = new int[k];
+        for(int i = 0 ; i<k ;i++){
             res[i] = pq.poll()[0];
         }
+
         return res;
-       
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int n : nums){
+//             map.put(n,map.getOrDefault(n,0)+1);
+//         }
+
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->a[1] - b[1]);
+
+//         for(int key : map.keySet()){
+//             int freq  = map.get(key);
+//             pq.add(new int[]{key,freq});
+
+//             if(pq.size()>k){
+
+//                 pq.poll();
+//             }
+//         }
+//         int res[] = new int[k];
+        
+
+//         for(int i = 0; i<k ; i++){
+//             res[i] = pq.poll()[0];
+//         }
+//         return res;
+       
+        
+//     }
+// }
