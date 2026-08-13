b403a02051bf2dc3a028254121d0bb42866a3f5d
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-13 13:12:41 +0530

    Time: 1 ms (99.67%), Space: 42.8 MB (70.38%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,27 +1,81 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
 
-        if(stones.length==1){
-            return stones[0];
-        }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // if(stones.length==1){
+        //     return stones[0];
+        // }
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int x : stones){
             pq.add(x);
         }
 
         while(pq.size()>1){
 
-        int x = pq.poll();
-        int y = pq.poll();
+            int x = pq.poll();
+            int y = pq.poll();
 
-        if(x > y){
-            pq.add(x-y);
+            if(x>y){
+                pq.add(x-y);
+            }
         }
-        }
-
         return pq.isEmpty() ? 0 : pq.peek();
-
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+
+//         if(stones.length==1){
+//             return stones[0];
+//         }
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+//         for(int x : stones){
+//             pq.add(x);
+//         }
+
+//         while(pq.size()>1){
+
+//         int x = pq.poll();
+//         int y = pq.poll();
+
+//         if(x > y){
+//             pq.add(x-y);
+//         }
+//         }
+
+//         return pq.isEmpty() ? 0 : pq.peek();
+
+//     }
+// }
