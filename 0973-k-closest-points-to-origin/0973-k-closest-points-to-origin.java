65120c9eae875ab910ce7d4fb172c533820c9d70
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-18 12:05:39 +0530

    Time: 22 ms (89.91%), Space: 51.9 MB (92.7%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,27 +1,88 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-
+        
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) ->{ int d1 = a[0] * a[0] + a[1] * a[1];
+            (a,b) -> {
+            int d1 = a[0] * a[0] + a[1] * a[1];
             int d2 = b[0] * b[0] + b[1] * b[1];
-            return d2-d1; }
+
+            return d1 - d2;
+            }
         );
 
-        for(int[] x : points){
+        for(int[] x : points ){
             pq.add(x);
-            if(pq.size()>k){
-                pq.poll();
-            }
         }
 
         int[][] res = new int[k][2];
 
-        for(int i = 0 ; i<k ;i++){
+        for(int i = 0 ; i < k ; i++){
             res[i] = pq.poll();
         }
+
         return res;
 
 
-        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[][] kClosest(int[][] points, int k) {
+
+//         PriorityQueue<int[]> pq = new PriorityQueue<>(
+//             (a,b) ->{ int d1 = a[0] * a[0] + a[1] * a[1];
+//             int d2 = b[0] * b[0] + b[1] * b[1];
+//             return d2-d1; }
+//         );
+
+//         for(int[] x : points){
+//             pq.add(x);
+//             if(pq.size()>k){
+//                 pq.poll();
+//             }
+//         }
+
+//         int[][] res = new int[k][2];
+
+//         for(int i = 0 ; i<k ;i++){
+//             res[i] = pq.poll();
+//         }
+//         return res;
+
+
+        
+//     }
+// }
