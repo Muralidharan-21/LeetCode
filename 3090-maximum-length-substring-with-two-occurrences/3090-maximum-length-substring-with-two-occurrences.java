c81d0958a0bc9ee0cf2b063c184db18c89f7d1b0
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-18 11:35:42 +0530

    Time: 1 ms (100%), Space: 43.6 MB (49.95%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,22 +1,61 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
 
-        int[] freq = new int[26];
-
+        int freq[] = new int[26];
+        int res = 0;
         int left = 0;
-        int ans = 0;
-
-        for(int right = 0 ;right<s.length() ; right++){
+        for(int right = 0 ; right < s.length() ; right++){
             freq[s.charAt(right)-'a']++;
             while(freq[s.charAt(right)-'a']>2){
                 freq[s.charAt(left)-'a']--;
                 left++;
             }
 
-            ans = Math.max(ans,right-left+1);
-
+            res = Math.max(res, right - left+1);
         }
-        return ans;
+
+        return res;
+
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maximumLengthSubstring(String s) {
+
+//         int[] freq = new int[26];
+
+//         int left = 0;
+//         int ans = 0;
+
+//         for(int right = 0 ;right<s.length() ; right++){
+//             freq[s.charAt(right)-'a']++;
+//             while(freq[s.charAt(right)-'a']>2){
+//                 freq[s.charAt(left)-'a']--;
+//                 left++;
+//             }
+
+//             ans = Math.max(ans,right-left+1);
+
+//         }
+//         return ans;
+        
+//     }
+// }
