caa4b8217a2630125baa542dee61a370ed8b6416
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-18 13:39:12 +0530

    Time: 5 ms (40.81%), Space: 47.1 MB (14.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,28 +11,103 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
 
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val - b.val);
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val - b.val);
 
         for(ListNode x : lists){
-            if(x !=null)
+            if(x!=null){
             pq.add(x);
+            }
         }
 
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        while(!pq.isEmpty()){
-            curr.next= pq.poll();
+        while(pq.size()>0){
+            curr.next = pq.poll();
             curr = curr.next;
 
             if(curr.next!=null){
                 pq.add(curr.next);
             }
-            
         }
-        return dummy.next;
-         
 
+        return dummy.next;
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val - b.val);
+
+//         for(ListNode x : lists){
+//             if(x !=null)
+//             pq.add(x);
+//         }
+
+//         ListNode dummy = new ListNode(-1);
+//         ListNode curr = dummy;
+
+//         while(!pq.isEmpty()){
+//             curr.next= pq.poll();
+//             curr = curr.next;
+
+//             if(curr.next!=null){
+//                 pq.add(curr.next);
+//             }
+            
+//         }
+//         return dummy.next;
+         
+
+        
+//     }
+// }
