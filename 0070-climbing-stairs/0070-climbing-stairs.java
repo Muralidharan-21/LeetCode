d32b112c128ef71d498f8de5e6047c2219fc8639
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-23 21:01:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,26 +1,50 @@
 class Solution {
     public int climbStairs(int n) {
 
+
+        if(n<=1){
+            return n;
+        }
         int[] dp = new int[n+1];
-        return solve(n,dp);
+
         
-    }
 
-    private int solve(int num,int dp[]){
+        dp[1] = 1;
+        dp[2] = 2;
 
-        if(num == 1){
-            return 1;
+        for(int i = 3 ; i<=n ;i++){
+            dp[i] = dp[i-1]+ dp[i-2];
         }
-        if(num == 2){
-            return 2;
-        }
-
-        if(dp[num]!=0){
-            return dp[num];
-        }
-
-        dp[num] =  solve(num-1,dp) + solve(num-2,dp);
-        return dp[num];
-
+        return dp[n];
     }
 }
+
+
+
+// Memorization
+// class Solution {
+//     public int climbStairs(int n) {
+
+//         int[] dp = new int[n+1];
+//         return solve(n,dp);
+        
+//     }
+
+//     private int solve(int num,int dp[]){
+
+//         if(num == 1){
+//             return 1;
+//         }
+//         if(num == 2){
+//             return 2;
+//         }
+
+//         if(dp[num]!=0){
+//             return dp[num];
+//         }
+
+//         dp[num] =  solve(num-1,dp) + solve(num-2,dp);
+//         return dp[num];
+
+//     }
+// }
