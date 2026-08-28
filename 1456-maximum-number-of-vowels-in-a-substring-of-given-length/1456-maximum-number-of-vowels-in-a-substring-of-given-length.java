550b23e9366c7904adfe7ff1f61280de1f643dba
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-28 09:57:36 +0530

    Time: 27 ms (8.75%), Space: 46.7 MB (17.71%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,29 +1,28 @@
 class Solution {
     public int maxVowels(String s, int k) {
-
-        String v = "aeiou";
-        int max = 0;
-        int c = 0;
+        
+        int vc = 0;
         int left = 0;
-        for(int right = 0 ;right<s.length();right++){
+        int res = 0;
+        String v = "aeiou";
 
+        for(int right =0 ; right<s.length() ;right++){
             if(v.contains(s.charAt(right)+"")){
-                c++;
+                vc++;
             }
-            if(right -left +1 == k){
-                max = Math.max(c,max);
+            while(right-left+1>k){
                 if(v.contains(s.charAt(left)+"")){
-                    c--;
+                    vc--;
                 }
                 left++;
             }
+            res = Math.max(res,vc);
 
         }
-        return max;
-        
-        
+        return res;
     }
 }
+
 
 
 
@@ -43,33 +42,26 @@ class Solution {
 // class Solution {
 //     public int maxVowels(String s, int k) {
 
-//         String vowel = "aeiou";
-
-//         int res = 0;
-//         int v_count = 0;
+//         String v = "aeiou";
+//         int max = 0;
+//         int c = 0;
 //         int left = 0;
+//         for(int right = 0 ;right<s.length();right++){
 
-//         for(int right = 0 ;right < s.length();right++){
-
-//            if(isVowel(s.charAt(right))){
-//                 v_count++;
+//             if(v.contains(s.charAt(right)+"")){
+//                 c++;
 //             }
-
-//             if(right-left+1 == k){
-//                 res = Math.max(res,v_count);
-                
-//                 if(isVowel(s.charAt(left))){
-//                     v_count--;
+//             if(right -left +1 == k){
+//                 max = Math.max(c,max);
+//                 if(v.contains(s.charAt(left)+"")){
+//                     c--;
 //                 }
 //                 left++;
 //             }
 
 //         }
-//         return res;
+//         return max;
         
-//     }
-
-//     public boolean isVowel(char ch){
-//         return "aeiou".indexOf(ch) != -1;
+        
 //     }
 // }
