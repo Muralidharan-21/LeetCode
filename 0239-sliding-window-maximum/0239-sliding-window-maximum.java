a13f470e71eab149d4349cd95a568772596fd87b
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-08-28 09:42:27 +0530

    Time: 501 ms (5%), Space: 152.7 MB (28.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,32 +1,40 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
+
         int left = 0;
         int n = nums.length;
         int[] res = new int[n-k+1];
         int p = 0;
         TreeMap<Integer,Integer> map = new TreeMap<>();
 
-        for(int right = 0 ; right<n ; right++){
-            
+        for(int right = 0 ;right<n ;right++){
             map.put(nums[right],map.getOrDefault(nums[right],0)+1);
 
             while(right-left+1>k){
                 map.put(nums[left],map.get(nums[left])-1);
-                if(map.get(nums[left])==0){
-                    map.remove(nums[left]);
+                if(map.get(nums[left]) == 0){
+                   map.remove(nums[left]);
+                   
                 }
                 left++;
+                
+
             }
+
             if(right-left+1==k){
                 res[p++] = map.lastKey();
             }
 
-        }
+        } 
         return res;
-
     }
 }
+
+
+
+
+
+
 
 
 
