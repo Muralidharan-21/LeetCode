5e3625257cdb1780446cacb6b4aaf723073cdee2
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-09-18 12:17:04 +0530

    Time: 2 ms (91.68%), Space: 47.7 MB (57.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,21 +3,34 @@ class Solution {
 
         int k = 0;
 
-        for(int i = 0; i<nums.length;i++){
-
-            int temp = 0;
+        for(int i = 0 ;i<nums.length;i++){
             if(nums[i]!=0){
-                temp = nums[k];
+                int temp = nums[k];
                 nums[k++] = nums[i];
-                nums[i] = temp;;
-
+                nums[i] = temp;
             }
         }
 
+        
 
+        
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -33,15 +46,21 @@ class Solution {
 //         int k = 0;
 
 //         for(int i = 0; i<nums.length;i++){
+
+//             int temp = 0;
 //             if(nums[i]!=0){
-//                 int temp = nums[i];
-//                 nums[i] = nums[k];
-//                 nums[k] = temp;
-//                 k++;
+//                 temp = nums[k];
+//                 nums[k++] = nums[i];
+//                 nums[i] = temp;;
+
 //             }
 //         }
-     
-        
 
+
+        
 //     }
 // }
+
+
+
+
