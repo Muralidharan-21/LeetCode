dd2c11ecb9e6f4dce09ab0499125fc8d3034fc7f
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-09-20 12:23:33 +0530

    Time: 1 ms (100%), Space: 43.7 MB (91.21%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+
+        int res = 0;
+
+        for(int i = 0 ; i<s.length() ; i++){
+            res+= (i+1) * ('z' - s.charAt(i) + 1);
+        }
+
+        return res;
+        
+    }
+}
