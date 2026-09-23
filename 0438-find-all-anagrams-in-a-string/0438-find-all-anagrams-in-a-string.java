5a6ac083f3dccfeb890a1f8d063bcdd3f26b0dfd
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-09-23 12:13:59 +0530

    Time: 1459 ms (5.03%), Space: 47.8 MB (12.43%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,39 +1,26 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
-        int pat[] = new int[26];
-        int win[] = new int[26];
-
         ArrayList<Integer> arr = new ArrayList<>();
-
-        for(char x : p.toCharArray()){
-            pat[x-'a']++;
-        }
-        int winSize = p.length();
-        if(p.length()>s.length()){
-            return arr;
-        }
-        for(int i = 0 ; i<winSize ; i++){
-            win[s.charAt(i)-'a']++;
-        }
-        if(Arrays.equals(pat,win)){
-            arr.add(0);
-        }
-
-        for(int i = winSize; i<s.length();i++){
-
-            win[s.charAt(i)-'a']++;
-            win[s.charAt(i-winSize)-'a']--;
-
-            if(Arrays.equals(pat,win)){
-                arr.add((i-winSize)+1);
+        char[] pp = p.toCharArray();
+        Arrays.sort(pp);
+        for(int i = 0; i<=s.length()-p.length() ;i++ ){
+            String ss = s.substring(i,i+p.length());
+            char[] ssa = ss.toCharArray();
+            Arrays.sort(ssa);
+            if(Arrays.equals(ssa,pp)){
+                arr.add(i);
             }
-
         }
         return arr;
         
     }
 }
+
+
+
+
+
 
 
 
