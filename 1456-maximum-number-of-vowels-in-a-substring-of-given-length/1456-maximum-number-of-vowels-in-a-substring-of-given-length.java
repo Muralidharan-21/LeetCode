4383b60be61609941785b750d3a8e396392e2d5f
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-09-23 10:36:13 +0530

    Time: 28 ms (7.09%), Space: 47.1 MB (6.11%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,25 +1,28 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        
-        int vc = 0;
-        int left = 0;
-        int res = 0;
-        String v = "aeiou";
 
-        for(int right =0 ; right<s.length() ;right++){
-            if(v.contains(s.charAt(right)+"")){
+        int vc = 0;
+        int max = 0;
+        int left = 0;
+        String vowel = "aeiou";
+        for(int right = 0 ; right<s.length() ;right++){
+
+            if(vowel.contains(s.charAt(right)+"")){
                 vc++;
             }
-            while(right-left+1>k){
-                if(v.contains(s.charAt(left)+"")){
+
+            if(right-left+1==k){
+                max = Math.max(vc,max);
+                if(vowel.contains(s.charAt(left)+"")){
                     vc--;
                 }
                 left++;
             }
-            res = Math.max(res,vc);
 
         }
-        return res;
+
+        return max;
+        
     }
 }
 
@@ -37,31 +40,27 @@ class Solution {
 
 
 
-
-
 // class Solution {
 //     public int maxVowels(String s, int k) {
-
-//         String v = "aeiou";
-//         int max = 0;
-//         int c = 0;
+        
+//         int vc = 0;
 //         int left = 0;
-//         for(int right = 0 ;right<s.length();right++){
+//         int res = 0;
+//         String v = "aeiou";
 
+//         for(int right =0 ; right<s.length() ;right++){
 //             if(v.contains(s.charAt(right)+"")){
-//                 c++;
+//                 vc++;
 //             }
-//             if(right -left +1 == k){
-//                 max = Math.max(c,max);
+//             while(right-left+1>k){
 //                 if(v.contains(s.charAt(left)+"")){
-//                     c--;
+//                     vc--;
 //                 }
 //                 left++;
 //             }
+//             res = Math.max(res,vc);
 
 //         }
-//         return max;
-        
-        
+//         return res;
 //     }
 // }
