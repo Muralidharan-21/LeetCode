2b9082beac9cf5714337d363bde9c6836c30a4e6
Murali Dharan R <muralidharanrajmohan@gmail.com>
2026-09-24 12:33:03 +0530

    Time: 7 ms (89.37%), Space: 46.3 MB (38.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-        int freq[] =new int[26];
-        int max = 0;
         int res = 0;
-
-
+        int maxFreq = 0;
+        int[] freq = new int[26];
         int left = 0;
-        for(int right = 0; right<s.length() ;right++){
 
-            char ch = s.charAt(right);
-            freq[ch-'A']++;
-            max = Math.max(freq[ch-'A'],max);
+        for(int right = 0 ; right<s.length() ; right++){
 
-            while( (right-left+1)-max >k ){
+            freq[s.charAt(right)-'A']++;
+            maxFreq = Math.max(maxFreq,freq[s.charAt(right)-'A']);
+
+
+            while( (right-left+1)-maxFreq > k ){
                 freq[s.charAt(left)-'A']--;
                 left++;
             }
-            res = Math.max((right-left+1),res);
+
+            res = Math.max(right-left+1,res);
+
+
         }
         return res;
         
@@ -25,51 +27,57 @@ class Solution {
 }
 
 
+// class Solution {
+//     public int characterReplacement(String s, int k) {
 
+        
+//         int ans = 0;
+      
+//         for(int i = 0 ; i<s.length();i++){
+//             int maxFreq = 0;
+//             int[] freq = new int[26];
+//             for(int j = i ; j<s.length() ; j++){
 
+//                 freq[s.charAt(j)-'A']++;
+//                 maxFreq = Math.max(maxFreq,freq[s.charAt(j)-'A']);
 
+//                 if((j-i+1)-maxFreq<=k){
+//                     ans = Math.max(ans,j-i+1);
+//                 }
+     
 
+//             }
 
-
-
-
-
-
-
-
-
-
-
+//         }
+//         return ans;
+        
+//     }
+// }
 
 
 
 // class Solution {
 //     public int characterReplacement(String s, int k) {
 
-//         int freq[] = new int[26];
-
-//         int left = 0;
-//         int max_freq = 0;
+//         int freq[] =new int[26];
+//         int max = 0;
 //         int res = 0;
 
-//         for(int right = 0; right<s.length();right++){
+
+//         int left = 0;
+//         for(int right = 0; right<s.length() ;right++){
 
 //             char ch = s.charAt(right);
-
 //             freq[ch-'A']++;
-//             max_freq = Math.max(freq[ch-'A'],max_freq);
+//             max = Math.max(freq[ch-'A'],max);
 
-//             while( (right-left+1) - max_freq >k){
+//             while( (right-left+1)-max >k ){
 //                 freq[s.charAt(left)-'A']--;
 //                 left++;
 //             }
-
-//             // freq[ch-'A']++;
-//             // max_freq = Math.max(freq[ch-'A'],max_freq);
-//             res = Math.max(right-left+1,res);
+//             res = Math.max((right-left+1),res);
 //         }
 //         return res;
-
         
 //     }
 // }
